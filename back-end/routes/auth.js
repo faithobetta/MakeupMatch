8cc1b1e73e3abc440db6440011296cdcc851fe4d
faith obetta <obettafaith24@gmail.com>
2024-08-13 05:35:13 +0100

    FetchArtistsByLoaction route Added

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { SignUpClient, Login } from '../controllers/authController.js';
-import { fetchArtistById, SignUpArtist } from '../controllers/artistController.js';
+import { fetchArtistById, fetchArtistsByLocation, SignUpArtist } from '../controllers/artistController.js';
 import { ArtistDashboard } from '../controllers/artistController.js';
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.post('/signUpArtist', SignUpArtist);
 router.post('/signUpClient', SignUpClient);
 router.post('/login', Login);
 router.get('/fetchArtist/:id', fetchArtistById);
+router.get('/fetchArtistsByLocation/:location', fetchArtistsByLocation);
 router.post('/artist-dashboard', ArtistDashboard);
 
 export default router;
